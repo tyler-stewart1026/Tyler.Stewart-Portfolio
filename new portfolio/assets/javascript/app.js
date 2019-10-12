d30c91d49a17483228be50fbf933d29e9a3a2588
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-10-11 19:29:26 -0600

    Wireframe, parallax, and portfolio sections framework laid out.

---
+++ new portfolio/assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.carousel').carousel();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.sidenav').sidenav();
+  $('.modal').modal();
+});
+
+
+M.textareaAutoResize($('#textarea1'));
