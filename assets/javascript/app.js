c851ed9ff9cfc9c98addda594d8d2a83e2a77f4a
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-11-02 16:44:59 -0600

    refactored entire code, new portfolio, new color scheme, new fonts, new everything, code clean up still needed, more changes to come to icons.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,53 @@
-M.textareaAutoResize($('#textarea1'));
-
-
-
-$(document).ready(function () {
-  $('.fixed-action-btn').floatingActionButton();
-  $('.modal').modal();
-  $('.parallax').parallax();
-
+ScrollReveal().reveal('.services1', {
+    delay: 900,
+    duration: 2000
 });
+ScrollReveal().reveal('.services2', {
+    delay: 1100,
+    duration: 2000
+});
+ScrollReveal().reveal('.services3', {
+    delay: 1400,
+    duration: 2000
+});
+ScrollReveal().reveal('.aboutPort', {
+    origin: 'left',
+    distance: '100px',
+    duration: 1000
+});
+ScrollReveal().reveal('.line-break2', {
+    origin: 'right',
+    distance: '100px',
+    delay: 500,
+    duration: 1000
+});
+
+ScrollReveal().reveal('.aboutMe', {
+    origin: 'right',
+    distance: '50px',
+    delay: 500,
+    duration: 1000
+})
+
+ScrollReveal().reveal('.profPic1', {
+    origin: 'left',
+    distance: '100px',
+    delay: 500,
+    duration: 1000
+})
+
+ScrollReveal().reveal('.row1', {
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1000,
+    duration: 1000
+})
+ScrollReveal().reveal('.row2', {
+    origin: 'bottom',
+    distance: '100px',
+    delay: 1000,
+    duration: 1500
+})
+
+ScrollReveal().reveal('.iconReveal', {
+})
